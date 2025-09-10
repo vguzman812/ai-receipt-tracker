1c4419fee64ade0134a8ccc3ac23a1805efac9d3
vguzman812 <vguzman812@gmail.com>
2025-09-10 15:02:13 -0400

    Enhance ManagePlan component with improved layout and user instructions
    - Updated ManagePlan component to include a container for better styling.
    - Added a header and description to guide users in managing their subscription and billing details.

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -5,7 +5,11 @@ const ManagePlan = () => {
     return <div>No component ID found</div>;
   }
   return (
-    <div>
+    <div className="container xl:max-w-5xl mx-auto p-4 md:p-0">
+      <h1 className="text-2xl font-bold mb-4 my-8">Manage Your Plan</h1>
+      <p className="text-gray-600 mb-8">
+        Manage your subscription and billing details here.
+      </p>
       <SchematicComponent
         componentId={
           process.env.NEXT_PUBLIC_SCHEMATIC_CUSTOMER_PORTAL_COMPONENT_ID
