9ee56e33049c1c48505ff86595b3cbf628a70c6b
vguzman812 <vguzman812@gmail.com>
2025-09-10 13:59:01 -0400

    Add @schematichq/schematic-react dependency and implement getTemporaryAccessToken action
    - Added @schematichq/schematic-react version 1.2.5 to package.json and pnpm-lock.yaml.
    - Created a new action, getTemporaryAccessToken, to issue temporary access tokens for users based on their company ID.
    - Removed unused inner and page components from the server directory to streamline the codebase.

---
+++ actions/getTemporaryAccessToken.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+// Initialize Schematic SDK
+import { SchematicClient } from "@schematichq/schematic-typescript-node";
+const apiKey = process.env.SCHEMATIC_API_KEY;
+const client = new SchematicClient({ apiKey });
+
+// Get a temporary access token
+export async function getTemporaryAccessToken(companyId: string) {
+  const user = await currentUser();
+  if (!user) {
+    return null
+  }
+  const resp = await client.accesstokens.issueTemporaryAccessToken({
+    resource_type: "company",
+    lookup: { id: user.id }, 
+  });
+  return resp.data?.token;
+}
